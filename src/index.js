bebda8886f89e18e693c5a28341d1077e7e83cd8
Sergio <sergio@nuvolar.eu>
2021-02-03 18:24:50 +0100

    Add recovery from errors

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ const TeleBot = require('telebot');
 const request = require('request');
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+console.log('Creating bot with token ' + TELEGRAM_BOT_TOKEN);
 const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('¡Ya estoy aquí!'));
-bot.on('/platillo', (msg) => {
-    return getPlatillo().then(function(result) {
-        msg.reply.text(result);
-    });
+bot.on(['/platillo'], (msg) => {
+    try {
+        return getPlatillo().then(function(result) {
+            msg.reply.text(result);
+        });
+    } catch(error) {
+        msg.reply.text('¡ÁNDELE!¡Algo salió mal!');
+    }
 });
 
 bot.start();
 
 async function getPlatillo() {
     let res = await doRequest();
-    return JSON.parse(res).dishName;
+    if (JSON.parse(res).dishName) {
+        return JSON.parse(res).dishName;
+    } else {
+        return '¡AY, AY, AY! ¡Me quedé atorado!';
+    }
 }
 
 function doRequest() {
